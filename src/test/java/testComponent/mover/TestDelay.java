c3283e18c46ae40fe6105ffd2228c3be5d3f352b
nontaphatfirm <87847182+nontaphatfirm@users.noreply.github.com>
2026-03-05 05:14:40 +0700

    Update TestDelay

--- src/test/java/testComponent/mover/TestDelay.java
+++ src/test/java/testComponent/mover/TestDelay.java
@@ -1,28 +1,51 @@
 package testComponent.mover;
 
+import component.card.Card;
+import component.card.Material;
+import component.card.Suit;
 import component.mover.Delay;
+import logic.GameLevel;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import util.Direction;
+import util.GridPos;
+import util.TestLevel;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestDelay {
     private Delay delay;
+    private GameLevel level;
+    private GridPos gridPos;
 
     @BeforeEach
     void setUp() {
+        level = TestLevel.initGlobalGameLevel();
+        gridPos = new GridPos(2, 2);
+
         delay = new Delay(Direction.DOWN);
+        delay.setGridPos(gridPos);
+        level.addMover(delay, gridPos);
     }
 
     @Test
     void testValidOutputDirections() {
         Direction[] validOutputDirections = delay.getValidOutputDirections();
-
         assertEquals(1, validOutputDirections.length);
         assertEquals(Direction.DOWN, validOutputDirections[0]);
     }
 
-    // Cannot test getDirection() & getDirectionStateless() here
-    // Delay Logic should test in Integration Test (TestGameLevel)
+    @Test
+    void testGetDirection() {
+        Card card1 = new Card(Suit.CLUB, 5, Material.PLASTIC);
+        level.addCard(card1, gridPos);
+        assertEquals(Direction.STAY, delay.getDirection());
+        assertEquals(Direction.DOWN, delay.getDirection());
+        level.removeCard(card1);
+
+        Card card2 = new Card(Suit.HEART, 2, Material.PLASTIC);
+        level.addCard(card2, gridPos);
+        assertEquals(Direction.STAY, delay.getDirection());
+        assertEquals(Direction.DOWN, delay.getDirection());
+    }
 }
